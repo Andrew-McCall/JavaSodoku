7e45a3f2a978c7057012b4b467a19a0a896e7cfc
Andrew McCall <amccall@qa.com>
2022-03-18 16:04:18 +0000

    Created Empty Board class and Dabbled in OOP again

---
+++ src/Board.java
@@ -0,0 +1,20 @@
+
+public class Board {
+	
+	public static int oopPrinciples = 1;
+
+	public int test;
+	
+	public Board(int x) {
+		
+		test = x;
+        int test = x;
+        
+        test -= 2;
+    }
+	
+	public void add(int y){
+		test += y;
+	}
+	
+}
--- src/Sodoku.java
+++ src/Sodoku.java
@@ -4,14 +4,21 @@ public class Sodoku {
 	
 	public static void main(String[] args) {
 		
-		JFrame window = new JFrame("Sodoku");
-
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        window.setResizable(false);
-        window.pack();
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
+//		JFrame window = new JFrame("Sodoku");
+//
+//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        window.setResizable(false);
+//        window.pack();
+//        window.setLocationRelativeTo(null);
+//        window.setVisible(true);
+//        
+        Board board = new Board(42);
+        System.out.println(board.test);
+        
+        board.add(-40);
+        
+        System.out.println(board.test);
         
 	}
 
